e0ca360cef63d332204d8e625d304c3c41ae0df7
Manasi670430 <manasijoshi1993@gmail.com>
2017-03-18 23:13:28 -0700

    Add files via upload

---
+++ PlantUMLInputStringBuilder.java
@@ -0,0 +1,28 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+import japa.parser.ast.body.FieldDeclaration;
+
+/**
+ * 
+ */
+
+/**
+ * @author Manasi Milind Joshi
+ *
+ */
+public class PlantUMLInputStringBuilder {
+
+	//OutputStream png = null;
+	//String source = "@startuml\n" ;
+	//source += "@enduml\n";
+
+	
+	public String generatePlantUMlStringInput(FieldDeclaration currentVariablItem)
+	{
+		return null;
+	
+			}
+
+}
