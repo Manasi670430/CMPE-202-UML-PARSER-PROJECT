5a3e438266bf32ae939c4d430e316d56ac3fd540
Manasi670430 <manasijoshi1993@gmail.com>
2017-03-24 02:07:47 -0700

    Update PlantUMLInputStringBuilder.java

--- PlantUMLInputStringBuilder.java
+++ PlantUMLInputStringBuilder.java
@@ -1,12 +1,15 @@
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import japa.parser.ast.body.FieldDeclaration;
+import net.sourceforge.plantuml.SourceStringReader;
 
-/**
- * 
- */
 
 /**
  * @author Manasi Milind Joshi
@@ -15,14 +18,62 @@ import japa.parser.ast.body.FieldDeclaration;
 public class PlantUMLInputStringBuilder {
 
 	//OutputStream png = null;
-	//String source = "@startuml\n" ;
+	String source = "@startuml\n" ;
+	//attributeDetail
 	//source += "@enduml\n";
-
 	
-	public String generatePlantUMlStringInput(FieldDeclaration currentVariablItem)
+	String imputStringForPlantUML = ""; 
+	
+	public void generatePlantUMlStringInput(HashMap<String, ClassTemplate> classTemplateMap, String imageFileName)
 	{
-		return null;
-	
+		
+		
+		
+		Iterator<Entry<String, ClassTemplate>> it = classTemplateMap.entrySet().iterator();
+		
+		
+		while(it.hasNext())
+		{
+			ClassTemplate classTemplate = it.next().getValue();
+			
+			
+			String classorInterfaceName = classTemplate.getCd().getName();
+			
+			if(classTemplate.getCd().isInterface)
+			{
+				imputStringForPlantUML = imputStringForPlantUML + "interface " + classorInterfaceName + "<<interface>>" + "{\n"; 
 			}
+			else
+			{
+				imputStringForPlantUML = imputStringForPlantUML + "class " + classorInterfaceName +  "{\n";
+			}
+			
+			imputStringForPlantUML += "}";
+			
+			
+		}
+		
+		
+		String finalString = source + imputStringForPlantUML + "\n@enduml";
+		SourceStringReader sr = new SourceStringReader(finalString);
+		FileOutputStream fos = null;
+		try {
+			fos =  new FileOutputStream(imageFileName + ".png");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		try {
+			sr.generateImage(fos);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+		//return null;
+
+	}
 
 }
