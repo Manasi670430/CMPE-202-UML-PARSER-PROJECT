fc6507b0f540e9d4a702e5f88a6a28d960136b44
Manasi670430 <manasijoshi1993@gmail.com>
2017-03-26 03:45:13 -0700

    Update PlantUMLInputStringBuilder.java

--- PlantUMLInputStringBuilder.java
+++ PlantUMLInputStringBuilder.java
@@ -6,10 +6,15 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 
 import japa.parser.ast.body.FieldDeclaration;
 import net.sourceforge.plantuml.SourceStringReader;
 
+
+/**
+ * 
+ */
 
 /**
  * @author Manasi Milind Joshi
@@ -18,14 +23,59 @@ import net.sourceforge.plantuml.SourceStringReader;
 public class PlantUMLInputStringBuilder {
 
 	//OutputStream png = null;
-	String source = "@startuml\n" ;
+	//String source = "@startuml" ;
 	//attributeDetail
 	//source += "@enduml\n";
 	
-	String imputStringForPlantUML = ""; 
+	String imputStringForPlantUML ;
 	
 	public void generatePlantUMlStringInput(HashMap<String, ClassTemplate> classTemplateMap, String imageFileName)
 	{
+		
+		
+		//System.out.println("In PLANT"+classTemplateMap);
+		//System.out.println(classTemplateMap.getClass());
+		String source = "@startuml" ;
+		Set set = classTemplateMap.entrySet();
+	      Iterator iterator = set.iterator();
+	      System.out.println("@startuml");
+	      while(iterator.hasNext()) {
+	         Map.Entry mentry = (Map.Entry)iterator.next();
+	         System.out.print("Class "+ mentry.getKey()+"\n");
+	         //imputStringForPlantUML ="Class"+ mentry.getKey()+"\n";
+	         //System.out.println(mentry.getValue());
+		
+	         String finalString = source + imputStringForPlantUML ;
+	 		SourceStringReader sr = new SourceStringReader(finalString);
+	 		FileOutputStream fos = null;
+	 		try {
+	 			fos =  new FileOutputStream(imageFileName + ".png");
+	 		} catch (FileNotFoundException e) {
+	 			// TODO Auto-generated catch block
+	 			e.printStackTrace();
+	 		}
+	 		
+	 		
+	 		try {
+	 			sr.generateImage(fos);
+	 		} catch (IOException e) {
+	 			// TODO Auto-generated catch block
+	 			e.printStackTrace();
+	 		}
+	      }
+	
+	//imputStringForPlantUML += "\n@enduml";
+    System.out.println("@enduml");
+    //System.out.println(imputStringForPlantUML);
+	
+	}
+	
+	
+	}
+	 		
+	/*	for(ClassTemplate a:classTemplateMap.values()){
+			System.out.println("Keyyyy:" + );
+	}
 		
 		
 		
@@ -51,10 +101,12 @@ public class PlantUMLInputStringBuilder {
 			imputStringForPlantUML += "}";
 			
 			
-		}
-		
-		
-		String finalString = source + imputStringForPlantUML + "\n@enduml";
+			System.out.println(imputStringForPlantUML);
+			
+			
+		}}
+		return null;
+	String finalString = source + imputStringForPlantUML + "\n@enduml";
 		SourceStringReader sr = new SourceStringReader(finalString);
 		FileOutputStream fos = null;
 		try {
@@ -71,9 +123,6 @@ public class PlantUMLInputStringBuilder {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	
-		//return null;
+		
+	*/
 
-	}
-
-}
