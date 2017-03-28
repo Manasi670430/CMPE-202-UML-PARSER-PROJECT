35c6b73ff4f690e04f9589e3251cb64f85596005
Manasi670430 <manasijoshi1993@gmail.com>
2017-03-27 23:35:24 -0700

    Add files via upload

---
+++ ClassDetails.java
@@ -0,0 +1,32 @@
+
+public class ClassDetails {
+
+	
+	
+	boolean isInterface;
+	String Name;
+	/**
+	 * @return the isInterface
+	 */
+	public boolean isInterface() {
+		return isInterface;
+	}
+	/**
+	 * @param isInterface the isInterface to set
+	 */
+	public void setInterface(boolean isInterface) {
+		this.isInterface = isInterface;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return Name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		Name = name;
+	}
+}
