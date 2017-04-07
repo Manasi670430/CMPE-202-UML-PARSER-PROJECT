3341f3d5873405e984ee007b402c02d3df97229d
Manasi670430 <manasijoshi1993@gmail.com>
2017-04-06 23:45:03 -0700

    Add files via upload

---
+++ ConnectionDetails.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+
+/**
+ * @author Manasi Milind Joshi
+ *
+ */
+public class ConnectionDetails {
+
+	String connectionStart;
+	String connectionEnd;
+	String connetionType;
+	public String getConnectionStart() {
+		return connectionStart;
+	}
+	public void setConnectionStart(String connectionStart) {
+		this.connectionStart = connectionStart;
+	}
+	public String getConnectionEnd() {
+		return connectionEnd;
+	}
+	public void setConnectionEnd(String connectionEnd) {
+		this.connectionEnd = connectionEnd;
+	}
+	public String getConnetionType() {
+		return connetionType;
+	}
+	public void setConnetionType(String connetionType) {
+		this.connetionType = connetionType;
+	}
+	
+	
+}
