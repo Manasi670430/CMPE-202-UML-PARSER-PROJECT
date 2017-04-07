eeebd61f1fe1f010f0608cbc4a5846fe6bb95885
Manasi670430 <manasijoshi1993@gmail.com>
2017-04-07 11:31:20 -0700

    Update PlantUMLInputStringBuilder.java

--- PlantUMLInputStringBuilder.java
+++ PlantUMLInputStringBuilder.java
@@ -2,19 +2,21 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
 import japa.parser.ast.body.FieldDeclaration;
+import japa.parser.ast.type.ClassOrInterfaceType;
+import japa.parser.ast.type.PrimitiveType;
+import japa.parser.ast.type.ReferenceType;
+import japa.parser.ast.type.Type;
 import net.sourceforge.plantuml.SourceStringReader;
 
-
-/**
- * 
- */
 
 /**
  * @author Manasi Milind Joshi
@@ -22,92 +24,58 @@ import net.sourceforge.plantuml.SourceStringReader;
  */
 public class PlantUMLInputStringBuilder {
 
-	//OutputStream png = null;
-	//String source = "@startuml" ;
-	//attributeDetail
-	//source += "@enduml\n";
+
 	
-	String imputStringForPlantUML ;
+	String finalString="";
+	String finalString1="";
+	String a="@startuml\n";
+	String b="@enduml";
+	
+	List<ConnectionDetails> connectionDetailList = new ArrayList<ConnectionDetails>();
 	
 	public void generatePlantUMlStringInput(HashMap<String, ClassTemplate> classTemplateMap, String imageFileName)
 	{
-		
-		
-		//System.out.println("In PLANT"+classTemplateMap);
-		//System.out.println(classTemplateMap.getClass());
-		String source = "@startuml" ;
+
+
+		String source = "@startuml\n" ;
 		Set set = classTemplateMap.entrySet();
-	      Iterator iterator = set.iterator();
-	      System.out.println("@startuml");
-	      while(iterator.hasNext()) {
-	         Map.Entry mentry = (Map.Entry)iterator.next();
-	         System.out.print("Class "+ mentry.getKey()+"\n");
-	         //imputStringForPlantUML ="Class"+ mentry.getKey()+"\n";
-	         //System.out.println(mentry.getValue());
-		
-	         String finalString = source + imputStringForPlantUML ;
-	 		SourceStringReader sr = new SourceStringReader(finalString);
-	 		FileOutputStream fos = null;
-	 		try {
-	 			fos =  new FileOutputStream(imageFileName + ".png");
-	 		} catch (FileNotFoundException e) {
-	 			// TODO Auto-generated catch block
-	 			e.printStackTrace();
-	 		}
-	 		
-	 		
-	 		try {
-	 			sr.generateImage(fos);
-	 		} catch (IOException e) {
-	 			// TODO Auto-generated catch block
-	 			e.printStackTrace();
-	 		}
-	      }
-	
-	//imputStringForPlantUML += "\n@enduml";
-    System.out.println("@enduml");
-    //System.out.println(imputStringForPlantUML);
-	
-	}
-	
-	
-	}
-	 		
-	/*	for(ClassTemplate a:classTemplateMap.values()){
-			System.out.println("Keyyyy:" + );
-	}
-		
-		
-		
-		Iterator<Entry<String, ClassTemplate>> it = classTemplateMap.entrySet().iterator();
-		
-		
-		while(it.hasNext())
-		{
-			ClassTemplate classTemplate = it.next().getValue();
+		Iterator iterator = set.iterator();
+		while(iterator.hasNext()) {
+			String inputStringForPlantUML= "" ;
+			Map.Entry mentry = (Map.Entry)iterator.next();
+			//imputStringForPlantUML ="Class "+ mentry.getKey()+"\n";
 			
 			
-			String classorInterfaceName = classTemplate.getCd().getName();
+			ClassTemplate cTemplate = (ClassTemplate)mentry.getValue();
 			
-			if(classTemplate.getCd().isInterface)
-			{
-				imputStringForPlantUML = imputStringForPlantUML + "interface " + classorInterfaceName + "<<interface>>" + "{\n"; 
-			}
-			else
-			{
-				imputStringForPlantUML = imputStringForPlantUML + "class " + classorInterfaceName +  "{\n";
-			}
+			inputStringForPlantUML += generateUMLClassOrInterfaceBody(cTemplate);
 			
-			imputStringForPlantUML += "}";
+			finalString += inputStringForPlantUML + "}\n" ;
 			
 			
-			System.out.println(imputStringForPlantUML);
+		}
+			
+		Iterator iterator1 = set.iterator();
+		while(iterator1.hasNext()) {
+			Map.Entry mentry = (Map.Entry)iterator1.next();
+			ClassTemplate cTemplate = (ClassTemplate)mentry.getValue();
+			
+			finalString += generateUMLConnections(cTemplate);
+			
+		}
 			
 			
-		}}
-		return null;
-	String finalString = source + imputStringForPlantUML + "\n@enduml";
-		SourceStringReader sr = new SourceStringReader(finalString);
+			
+			
+			
+			
+			finalString1 += finalString + "\n" ;
+			//finalString + "}\n";
+			//finalString1 =	a + finalString + b + "}\n"; 
+
+		System.out.println(finalString1);
+		finalString1 = a + finalString1 + b;
+		SourceStringReader sr = new SourceStringReader(finalString1);
 		FileOutputStream fos = null;
 		try {
 			fos =  new FileOutputStream(imageFileName + ".png");
@@ -115,14 +83,71 @@ public class PlantUMLInputStringBuilder {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
 		try {
 			sr.generateImage(fos);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
+
+	}
+	
+	
+	
+	public String generateUMLClassOrInterfaceBody(ClassTemplate cTemplate)
+	{
+		String umlClassInterfaceBody = "";
 		
-	*/
+		ClassDetails cDetails = cTemplate.getCd();
+		if(cDetails.isInterface)
+		{
+			umlClassInterfaceBody += "<<interface>> interface"+ cDetails.getName() +"\n";
+		}
+		else
+		{
+			umlClassInterfaceBody += "Class "+ cDetails.getName() +"{\n";
+		}
+		
+		List<AttributeDetails> attributeDetails = cTemplate.getListOfFields();
+		
+		for(AttributeDetails eachAttribute : attributeDetails)
+		{
+			if(eachAttribute.getType() instanceof ReferenceType)
+			{
+				if(eachAttribute.getType().toString().equals("int[]") || eachAttribute.getType().toString().equals("String[]"))
+				{
+					if(eachAttribute.getModifier().equals("Private"))
+					{
+						umlClassInterfaceBody += "-"+ eachAttribute.getName() + ":" + ((ReferenceType)eachAttribute.getType()).getType() + "[*]" +"\n";
+					}
+					if(eachAttribute.getModifier().equals("Public"))
+					{
+						umlClassInterfaceBody += "+" + eachAttribute.getName() + ":" + ((ReferenceType)eachAttribute.getType()).getType() + "[*]" + "\n";																				
+					}
+				}
+			}
+			if(eachAttribute.getType() instanceof PrimitiveType)
+			{
+				if(eachAttribute.getModifier().equals("Private"))
+				{
+					umlClassInterfaceBody += "-"+ eachAttribute.getName() + ":" + eachAttribute.getType() + "\n";
+				}
+				if(eachAttribute.getModifier().equals("Public"))
+				{
+					umlClassInterfaceBody += "+" + eachAttribute.getName() + ":" + eachAttribute.getType() + "\n";																				
+				}
+			}
+		}
+		
+		
+		
+		return umlClassInterfaceBody;
+	}
+	
+
+
+}
+
+
 
