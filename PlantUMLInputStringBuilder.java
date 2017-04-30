d6df36ef46fa8c4f8d83a7d03575ac9f9dc03e52
Manasi670430 <manasijoshi1993@gmail.com>
2017-04-29 23:40:51 -0700

    Finalized string for plant UML

--- PlantUMLInputStringBuilder.java
+++ PlantUMLInputStringBuilder.java
@@ -10,16 +10,14 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
-
 import japa.parser.ast.body.FieldDeclaration;
 import japa.parser.ast.body.Parameter;
+import japa.parser.ast.stmt.ExpressionStmt;
 import japa.parser.ast.type.ClassOrInterfaceType;
 import japa.parser.ast.type.PrimitiveType;
 import japa.parser.ast.type.ReferenceType;
 import japa.parser.ast.type.Type;
 import net.sourceforge.plantuml.SourceStringReader;
-
-
 /**
  * @author Manasi Milind Joshi
  *
@@ -34,38 +32,28 @@ public class PlantUMLInputStringBuilder {
 
 	public void generatePlantUMlStringInput(HashMap<String, ClassTemplate> classTemplateMap, String imageFileName, List<ClassTemplate> ctList)
 	{
-
-
 		String source = "@startuml\n" ;
 		Set set = classTemplateMap.entrySet();
 		Iterator iterator = set.iterator();
+
 		while(iterator.hasNext()) {
 			String inputStringForPlantUML= "" ;
 			Map.Entry mentry = (Map.Entry)iterator.next();
 			ClassTemplate cTemplate = (ClassTemplate)mentry.getValue();
-
 			inputStringForPlantUML += generateUMLClassOrInterfaceBody(cTemplate, ctList);
-
 			finalString += inputStringForPlantUML + "}\n" ;
-
-
 		}
 
 		Iterator iterator1 = set.iterator();
 		while(iterator1.hasNext()) {
 			Map.Entry mentry = (Map.Entry)iterator1.next();
 			ClassTemplate cTemplate = (ClassTemplate)mentry.getValue();
-
 			if(!cTemplate.getCd().isInterface())
 			{
 				finalString += generateUMLConnections(cTemplate,ctList);
 			}
-
-
 		}
-
 		finalString1 += finalString + "\n" ;
-
 		System.out.println(finalString1);
 		finalString1 = a + "skinparam classAttributeIconSize 0\n" + finalString1 + b;
 		SourceStringReader sr = new SourceStringReader(finalString1);
@@ -82,11 +70,7 @@ public class PlantUMLInputStringBuilder {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
-
 	}
-
-
 
 	public String generateUMLClassOrInterfaceBody(ClassTemplate cTemplate, List<ClassTemplate> ctList)
 	{
@@ -111,7 +95,6 @@ public class PlantUMLInputStringBuilder {
 				{
 					if(eachAttribute.getType().toString().equals("int[]") || eachAttribute.getType().toString().equals("String[]"))
 					{
-
 						if(eachAttribute.getModifier().equalsIgnoreCase("Private"))
 						{
 							umlClassInterfaceBody += "-"+ eachAttribute.getName() + ":" + ((ReferenceType)eachAttribute.getType()).getType() + "[*]" +"\n";
@@ -148,14 +131,50 @@ public class PlantUMLInputStringBuilder {
 		}
 
 		List<MethodDetails> methodDetailsList = cTemplate.getListOfMethods();
+		if(methodDetailsList!=null)
+		{
+			for(MethodDetails eachMethod : methodDetailsList)
+			{
+				Parameter params = eachMethod.getParamaters();
+
+				if(eachMethod.getModifier().equals("public"))
+				{
+					if(params!=null)
+					{
+						umlClassInterfaceBody += "+"+ eachMethod.getName() + "(" + params.getId().toString() + ":" + params.getType().toString() + ") :" + eachMethod.getReturnType().toString() + "\n";
+					}
+					else
+					{
+						umlClassInterfaceBody += "+"+ eachMethod.getName() + "() :" + eachMethod.getReturnType().toString() + "\n";
+					}
+				}
+				if(eachMethod.getModifier().equals("public abstract"))
+				{
+					if(params!=null)
+					{
+						umlClassInterfaceBody += "+"+ eachMethod.getName() + "(" + params.getId().toString() + ":" + params.getType().toString() + ") :" + eachMethod.getReturnType().toString() + "\n";
+					}
+					else
+					{
+						umlClassInterfaceBody += "+"+ eachMethod.getName() + "() :" + eachMethod.getReturnType().toString() + "\n";
+					}
+				}
+				if(eachMethod.getModifier().equals("public static"))
+				{
+					if(params!=null)
+					{
+						umlClassInterfaceBody += "+"+ eachMethod.getName() + "(" + params.getId().toString() + ":" + params.getType().toString() + ") :" + eachMethod.getReturnType().toString() + "\n";
+					}
+				}
+			}
+		}
+
 		List<ConstructorDetails> consDetails = cTemplate.getListOfConstructor();
 		if(consDetails!=null)
 		{
 			for(ConstructorDetails eachConstructor : consDetails)
 			{
 				Parameter params = eachConstructor.getConstructorParams();
-
-
 				if(params!=null)
 				{
 					umlClassInterfaceBody += "+"+ eachConstructor.getConstructorName() + "(" + params.getId().toString() + ":" + params.getType().toString() + ")" +  "\n";
@@ -166,8 +185,6 @@ public class PlantUMLInputStringBuilder {
 				}
 			}
 		}
-
-
 		return umlClassInterfaceBody;
 	}
 
@@ -176,7 +193,7 @@ public class PlantUMLInputStringBuilder {
 		String umlConnectionsString = "";
 		List<AttributeDetails> attributeDetailsList = cTemplate.getListOfFields();
 		List<MethodDetails> methodDetailsList = cTemplate.getListOfMethods();
-
+		List<ConstructorDetails> constDetailsList = cTemplate.getListOfConstructor();
 		if(attributeDetailsList!=null)
 		{
 			for(AttributeDetails eachAttribute : attributeDetailsList)
@@ -186,7 +203,6 @@ public class PlantUMLInputStringBuilder {
 					if(((ReferenceType)eachAttribute.getType()).getType() instanceof ClassOrInterfaceType)
 					{
 						String typeCollectionOrOnlyClass = ((ClassOrInterfaceType)((ReferenceType)eachAttribute.getType()).getType()).getName().toString();
-
 						if(!typeCollectionOrOnlyClass.equals("String"))
 						{
 							String associationEndClass = "";
@@ -196,15 +212,11 @@ public class PlantUMLInputStringBuilder {
 								List<Type> associationEndClassTypeList = ((ClassOrInterfaceType)((ReferenceType)eachAttribute.getType()).getType()).getTypeArgs();
 								associationEndClass = associationEndClassTypeList.get(0).toString();
 								associationStartClass = cTemplate.getCd().getName().toString();
-
 								boolean connectionExistFlag = false;
-
 								for(ConnectionDetails eachConnection : connectionDetailList)
 								{
 									boolean associationStartClassExist = associationStartClass.equals(eachConnection.getConnectionStart()) || associationStartClass.equals(eachConnection.getConnectionEnd());
 									boolean associationEndClassExist = associationEndClass.equals(eachConnection.getConnectionEnd()) || associationEndClass.equals(eachConnection.getConnectionStart());
-
-
 									if(associationStartClassExist && associationEndClassExist)
 									{
 										boolean connectionExist = eachConnection.getConnetionType()!=null && eachConnection.getConnetionType().toString().equals("ASSOCIATION");
@@ -212,7 +224,6 @@ public class PlantUMLInputStringBuilder {
 										{
 											connectionExistFlag = true;
 										}
-
 									}
 								}
 								if(!connectionExistFlag)
@@ -225,41 +236,172 @@ public class PlantUMLInputStringBuilder {
 
 									umlConnectionsString += associationEndClass + "\"*\"" + " -- " + "\"1\"" + cTemplate.getCd().getName() + "\n";
 								}
-								if(methodParams!=null)
-				{
-					String dependencyDestinationClass = methodParams.getType().toString();
-					String dependencySourceClass = cTemplate.getCd().getName();
+							}
+							else
+							{
+								boolean connectionExistFlag = false;
+								associationEndClass = ((ClassOrInterfaceType)((ReferenceType)eachAttribute.getType()).getType()).getName().toString();
+								associationStartClass = cTemplate.getCd().getName().toString();
 
-
-					ConnectionDetails connectionDetails = new ConnectionDetails();
-					connectionDetails.setConnectionStart(dependencySourceClass);
-					connectionDetails.setConnectionEnd(dependencyDestinationClass);
-					connectionDetails.setConnetionType("DEPENDENCY");
-					connectionDetailList.add(connectionDetails);
-					umlConnectionsString += dependencyDestinationClass + "<.." + dependencySourceClass + "\n";
-
-						}
-					}
-					if(constDetailsList!=null)
-		{
-		for(ConstructorDetails eachM : constDetailsList)
-		{
-			Parameter methodParams = eachM.getConstructorParams();
-
-
-								if(dependencySourceClassExist && dependencyDestinationClassExist)
+								for(ConnectionDetails eachConnection : connectionDetailList)
 								{
-									boolean connectionExist = eachConnection.getConnetionType()!=null && eachConnection.getConnetionType().toString().equals("DEPENDENCY");
-									if(connectionExist)
+									boolean associationStartClassExist = associationStartClass.equals(eachConnection.getConnectionStart()) || associationStartClass.equals(eachConnection.getConnectionEnd());
+									boolean associationEndClassExist = associationEndClass.equals(eachConnection.getConnectionEnd()) || associationEndClass.equals(eachConnection.getConnectionStart());
+									if(associationStartClassExist && associationEndClassExist)
 									{
-										connectionExistFlag = true;
+										boolean connectionExist = eachConnection.getConnetionType()!=null && eachConnection.getConnetionType().toString().equals("ASSOCIATION");
+										if(connectionExist)
+										{
+											connectionExistFlag = true;
+										}
 									}
-
+								}
+								if(!connectionExistFlag)
+								{
+									ConnectionDetails connectionDetails = new ConnectionDetails();
+									connectionDetails.setConnectionStart(associationStartClass);
+									connectionDetails.setConnectionEnd(associationEndClass);
+									connectionDetails.setConnetionType("ASSOCIATION");
+									connectionDetailList.add(connectionDetails);
+									umlConnectionsString += associationEndClass + "\"1\"" + " -- " + "\"1\"" + cTemplate.getCd().getName() + "\n";
 								}
 							}
+						}
+					}
+				}
+			}
+		}
+		if(constDetailsList!=null)
+		{
+			for(ConstructorDetails eachM : constDetailsList)
+			{
+				Parameter methodParams = eachM.getConstructorParams();
+				for(ClassTemplate eachClass : ctList)
+				{
+					if(methodParams!=null){
+						if(methodParams.getType().toString().equals(eachClass.getCd().getName()))
+						{
+							if(eachClass.getCd().isInterface())
+							{
+								boolean connectionExistFlag = false;
+								String dependencyDestinationClass = methodParams.getType().toString();
+								String dependencySourceClass = cTemplate.getCd().getName();
+								for(ConnectionDetails eachConnection : connectionDetailList)
+								{
+									boolean dependencySourceClassExist = dependencySourceClass.equals(eachConnection.getConnectionStart()) || dependencySourceClass.equals(eachConnection.getConnectionEnd());
+									boolean dependencyDestinationClassExist = dependencyDestinationClass.equals(eachConnection.getConnectionEnd()) || dependencyDestinationClass.equals(eachConnection.getConnectionStart());
+									if(dependencySourceClassExist && dependencyDestinationClassExist)
+									{
+										boolean connectionExist = eachConnection.getConnetionType()!=null && eachConnection.getConnetionType().toString().equals("DEPENDENCY");
+										if(connectionExist)
+										{
+											connectionExistFlag = true;
+										}
+									}
+								}
+								if(!connectionExistFlag)
+								{
+									ConnectionDetails connectionDetails = new ConnectionDetails();
+									connectionDetails.setConnectionStart(dependencySourceClass);
+									connectionDetails.setConnectionEnd(dependencyDestinationClass);
+									connectionDetails.setConnetionType("DEPENDENCY");
+									connectionDetailList.add(connectionDetails);
+									umlConnectionsString += dependencyDestinationClass + "<.. " + dependencySourceClass + "\n";
+								}
+							}
+						}
+					}
+				}
+			}
+
 		}
 
-							
+		if(methodDetailsList!=null)
+		{
+			for(MethodDetails eachMethod : methodDetailsList)
+			{
+				Parameter methodParams = eachMethod.getParamaters();
+				if(eachMethod.getName().equals("main"))
+				{
+					String bodyString = ((ExpressionStmt) eachMethod.getBodyElements().get(0)).toString();
+					System.out.println("BODYSTRING"+bodyString);
+					String bodyStmtArr[] = bodyString.split("=");
+					String interfaceName = bodyStmtArr[0].split(" ")[0];
+					for(ClassTemplate eachClass : ctList)
+					{
+						if(interfaceName.equals(eachClass.getCd().getName()))
+						{
+							if(eachClass.getCd().isInterface())
+							{
+								boolean connectionExistFlag = false;
+								String dependencyDestinationClass = interfaceName;
+								String dependencySourceClass = cTemplate.getCd().getName();
+								for(ConnectionDetails eachConnection : connectionDetailList)
+								{
+									boolean dependencySourceClassExist = dependencySourceClass.equals(eachConnection.getConnectionStart()) || dependencySourceClass.equals(eachConnection.getConnectionEnd());
+									boolean dependencyDestinationClassExist = dependencyDestinationClass.equals(eachConnection.getConnectionEnd()) || dependencyDestinationClass.equals(eachConnection.getConnectionStart());
+									if(dependencySourceClassExist && dependencyDestinationClassExist)
+									{
+										boolean connectionExist = eachConnection.getConnetionType()!=null && eachConnection.getConnetionType().toString().equals("DEPENDENCY");
+										if(connectionExist)
+										{
+											connectionExistFlag = true;
+										}
+									}
+								}
+								if(!connectionExistFlag)
+								{
+									ConnectionDetails connectionDetails = new ConnectionDetails();
+									connectionDetails.setConnectionStart(dependencySourceClass);
+									connectionDetails.setConnectionEnd(dependencyDestinationClass);
+									connectionDetails.setConnetionType("DEPENDENCY");
+									connectionDetailList.add(connectionDetails);
+									umlConnectionsString += dependencyDestinationClass + "<.." + dependencySourceClass + "\n";
+								}
+							}
+						}
+					}
+				}
+				for(ClassTemplate eachClass : ctList)
+				{
+					if(methodParams!=null)
+					{
+						if(methodParams.getType().toString().equals(eachClass.getCd().getName()))
+						{
+							if(eachClass.getCd().isInterface())
+							{
+								boolean connectionExistFlag = false;
+								String dependencyDestinationClass = methodParams.getType().toString();
+								String dependencySourceClass = cTemplate.getCd().getName();
+								for(ConnectionDetails eachConnection : connectionDetailList)
+								{
+									boolean dependencySourceClassExist = dependencySourceClass.equals(eachConnection.getConnectionStart()) || dependencySourceClass.equals(eachConnection.getConnectionEnd());
+									boolean dependencyDestinationClassExist = dependencyDestinationClass.equals(eachConnection.getConnectionEnd()) || dependencyDestinationClass.equals(eachConnection.getConnectionStart());
+									if(dependencySourceClassExist && dependencyDestinationClassExist)
+									{
+										boolean connectionExist = eachConnection.getConnetionType()!=null && eachConnection.getConnetionType().toString().equals("DEPENDENCY");
+										if(connectionExist)
+										{
+											connectionExistFlag = true;
+										}
+									}
+								}
+								if(!connectionExistFlag)
+								{
+									ConnectionDetails connectionDetails = new ConnectionDetails();
+									connectionDetails.setConnectionStart(dependencySourceClass);
+									connectionDetails.setConnectionEnd(dependencyDestinationClass);
+									connectionDetails.setConnetionType("DEPENDENCY");
+									connectionDetailList.add(connectionDetails);
+									umlConnectionsString += dependencyDestinationClass + "<.." + dependencySourceClass + "\n";
+								}
+							}
+						}
+					}
+				}
+			}
+			List<ClassOrInterfaceType> extendsList = cTemplate.getCd().getExtendsList();
+			List<ClassOrInterfaceType> implementsList = cTemplate.getCd().getImplementsList();
 			if(extendsList!=null)
 			{
 				for(ClassOrInterfaceType extendDef : extendsList)
@@ -277,7 +419,6 @@ public class PlantUMLInputStringBuilder {
 					umlConnectionsString += extendDestinationClass + "<|-- " + extendSourceClass + "\n";
 				}
 			}
-
 			if(implementsList!=null)
 			{
 				for(ClassOrInterfaceType implementsDef : implementsList)
@@ -296,13 +437,7 @@ public class PlantUMLInputStringBuilder {
 					umlConnectionsString += implementsDestinationClass + "<|.. " + implementsSourceClass + "\n";
 				}
 			}
-			}
 		}
+		return umlConnectionsString;
 	}
-
 }
-
-
-
-
-
